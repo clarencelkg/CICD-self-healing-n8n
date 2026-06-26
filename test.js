8c6f6ff4d3c35a4fd4c56865baf1a0d1f01b58be
clarencelkg <54013273+clarencelkg@users.noreply.github.com>
2026-06-26 18:14:46 +0800

    fix: Initialize Express app instance

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const http = require('http');
 
-
+const app = express(); // Initialize express app
 
 app.get('/', (req, res) => res.send('Hello from Express App'));
 
